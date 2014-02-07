adcb82de9f4da1f0d73a795876e975fb7d6d8b0d
jimbog <jimmygarzon@gmail.com>
2014-02-07 15:31:38 -0600

    add token field and generate random token

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,4 +1,5 @@
 class Word < ActiveRecord::Base
+  before_create { generate_token(:token) }
   has_many :votes, dependent: :destroy
   belongs_to :author
   accepts_nested_attributes_for :author
@@ -14,5 +15,10 @@ class Word < ActiveRecord::Base
 
   def down_votes
     self.votes.where(value: -1).count
+  end
+
+
+  def generate_token(column)
+      self[column] = SecureRandom.urlsafe_base64
   end
 end
